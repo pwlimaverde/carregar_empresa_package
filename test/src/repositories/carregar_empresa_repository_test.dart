f391fb336d2a40d7473e3996f22a5fd8dd9e79b8
PWLIMAVERDE <vorfast@gmail.com>
2021-02-08 22:48:56 -0300

    repositorio ok

--- test/src/repositories/carregar_empresa_repository_test.dart
+++ test/src/repositories/carregar_empresa_repository_test.dart
@@ -1,26 +1,28 @@
+import 'package:carregar_empresa_package/src/repositories/carregar_empresa_repository.dart';
 import 'package:carregar_empresa_package/src/usecases/carregar_empresa_usecase.dart';
 import 'package:carregar_empresa_package/src/usecases/entities/resultado_empresa.dart';
-import 'package:carregar_empresa_package/src/utilitarios/erros_carregar_temas.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:retorno_sucesso_ou_erro_package/retorno_sucesso_ou_erro_package.dart';
 import 'package:rxdart/rxdart.dart';
 
-class CarregarEmpresaRepositorioMock extends Mock
-    implements Repositorio<Stream<ResultadoEmpresa>, NoParams> {}
+class FairebaseEmpresaDatasource extends Mock
+    implements Datasource<Stream<ResultadoEmpresa>, NoParams> {}
 
 void main() {
+  late Datasource<Stream<ResultadoEmpresa>, NoParams> datasource;
   late Repositorio<Stream<ResultadoEmpresa>, NoParams> repositorio;
   late UseCase<Stream<ResultadoEmpresa>, NoParams> carregarEmpresaUsecase;
   late TempoExecucao tempo;
 
   setUp(() {
     tempo = TempoExecucao();
-    repositorio = CarregarEmpresaRepositorioMock();
+    datasource = FairebaseEmpresaDatasource();
+    repositorio = CarregarEmpresaRepositorio(datasource: datasource);
     carregarEmpresaUsecase = CarregarEmpresaUsecase(repositorio: repositorio);
   });
 
-  test('Deve retornar um sucesso com Stream<ResultadoEmpresa>', () async {
+  test('Deve retornar um sucesso com true', () async {
     tempo.iniciar();
     final testeFire = BehaviorSubject<ResultadoEmpresa>();
     testeFire.add(
@@ -30,8 +32,7 @@ void main() {
         licenca: true,
       ),
     );
-    when(repositorio).calls(#call).thenAnswer((_) => Future.value(
-        SucessoRetorno<Stream<ResultadoEmpresa>>(resultado: testeFire)));
+    when(datasource).calls(#call).thenAnswer((_) => Future.value(testeFire));
     final result = await carregarEmpresaUsecase(parametros: NoParams());
     print("teste result - ${await result.fold(
           sucesso: (value) => value.resultado,
@@ -51,7 +52,7 @@ void main() {
   });
 
   test(
-      'Deve retornar um ErrorCarregarEmpresa com Erro ao carregar os dados da empresa Cod.01-1',
+      'Deve ErrorCarregarEmpresa com Erro ao carregar os dados da empresa Cod.02-1',
       () async {
     tempo.iniciar();
     final testeFire = BehaviorSubject<ResultadoEmpresa>();
@@ -62,40 +63,7 @@ void main() {
         licenca: true,
       ),
     );
-    when(repositorio).calls(#call).thenAnswer(
-          (_) => Future.value(
-            ErroRetorno<Stream<ResultadoEmpresa>>(
-              erro: ErrorCarregarEmpresa(
-                mensagem: "Erro ao carregar os dados da empresa Cod.01-1",
-              ),
-            ),
-          ),
-        );
-    final result = await carregarEmpresaUsecase(parametros: NoParams());
-    print("teste result - ${await result.fold(
-      sucesso: (value) => value.resultado,
-      erro: (value) => value.erro,
-    )}");
-    tempo.terminar();
-    print(
-        "Tempo de Execução do CarregarEmpresa: ${tempo.calcularExecucao()}ms");
-    expect(result, isA<ErroRetorno<Stream<ResultadoEmpresa>>>());
-    testeFire.close();
-  });
-
-  test(
-      'Deve retornar um ErrorCarregarEmpresa com Erro ao carregar os dados da empresa Cod.01-1',
-      () async {
-    tempo.iniciar();
-    final testeFire = BehaviorSubject<ResultadoEmpresa>();
-    testeFire.add(
-      ResultadoEmpresa(
-        nome: "VorFast",
-        logo260x200: "logo",
-        licenca: true,
-      ),
-    );
-    when(repositorio).calls(#call).thenThrow(Exception());
+    when(datasource).calls(#call).thenThrow(Exception());
     final result = await carregarEmpresaUsecase(parametros: NoParams());
     print("teste result - ${await result.fold(
       sucesso: (value) => value.resultado,
