e86866ebabce201eb7034d4bb6313266ca40b510
PWLIMAVERDE <vorfast@gmail.com>
2021-03-07 11:34:35 -0300

    hotfix 1.1.1

--- lib/carregar_empresa_package.dart
+++ lib/carregar_empresa_package.dart
@@ -1,4 +1,4 @@
 library carregar_empresa_package;
 
 export 'src/presenter/carregar_empresa_presenter.dart';
-export 'src/usecases/entities/resultado_empresa.dart';
+export 'src/entities/resultado_empresa.dart';
